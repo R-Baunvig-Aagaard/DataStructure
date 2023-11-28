e7e29615f127c65a50661d0770c4e3d42efa4d34
Rasmus AA <rasmus-baunvig@live.dk>
2023-11-28 12:39:45 +0100

    Added MyArray class and method to insert value at last index

---
+++ DOA/DOA/Datatype/MyArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOA.Datatype;
+
+public class MyArray
+{
+    private int _realLength = 0;
+    private int[] _intArray;
+    public MyArray(int value)
+    {
+        _intArray = new int[value];
+    }
+    public void InitArrayExceptLast()
+    {
+        for (int i = 0; i < _intArray.Length - 1; i++)
+        {
+            _intArray[i] = i + 1;
+            _realLength++;
+
+        }
+        
+    }
+    public void InsertLast(int value)
+    {
+        _intArray[_realLength] = value;
+        _realLength++;
+        
+    }
+
+    public int ReturnLength()
+    {
+        Console.WriteLine($"Length: {_realLength}");
+        return _realLength;
+    }
+
+    public void PrintArray()
+    {
+        foreach (int i in _intArray)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
---
+++ DOA/DOA/Program.cs
@@ -0,0 +1,25 @@
+﻿
+/*  Array
+    type[] arrayName; 
+ */
+using DOA.Datatype;
+
+// New int array size/length/capacity of 5
+MyArray myArray = new MyArray(5);
+
+// Array Capacity
+//Console.WriteLine(intArray.Length);
+
+// Add values to all but last index
+myArray.InitArrayExceptLast();
+myArray.ReturnLength();
+
+// Print Array
+myArray.PrintArray();
+
+// Insert value at end of array
+myArray.InsertLast(9);
+myArray.ReturnLength();
+
+// Print Array
+myArray.PrintArray();
